15609090800fdf59cd479951c213b4d05bf53766
Bhuwan Sharma <69743169+bhuwanhike@users.noreply.github.com>
2025-07-24 19:36:22 +0530

    Delete startup directory

--- startup/backend/Route/addstartup.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const Router = express.Router();
-import startup from "../controller/addstartup.controller.js";
-
-Router.route("/addstartup").post(startup.addStartupController);
-
-Router.route("/startups").get(startup.getStartups);
-export default Router;
--- startup/backend/Route/login.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const Router = express.Router();
-import registerController from "../controller/login.controller.js";
-
-Router.route("/login").post(registerController);
-
-export default Router;
